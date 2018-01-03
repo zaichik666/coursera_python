d1c700f95cc05276ebf17174f3237636598079f7
zaichik666 <mary.a.sit@gmail.com>
2018-01-03 14:29:58 +0300

    05.07 36 = 2 * (3 * 6)

--- test.py
+++ test.py
@@ -1,6 +1,4 @@
-# 05.06
-n = int(input())
-for i in range(n):
-    for j in range(i + 1):
-        print(j + 1, sep='', end='')
-    print()
+# 05.07
+for i in range(10, 100):
+    if i == 2 * ((i // 10) * (i % 10)):
+        print(i, end=' ')
