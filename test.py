fcfb91114696670db7f870ff0f6ecfaf72c85f48
zaichik666 <mary.a.sit@gmail.com>
2018-01-03 14:46:44 +0300

    05.08 диофантово уравнение

--- test.py
+++ test.py
@@ -1,4 +1,13 @@
-# 05.07
-for i in range(10, 100):
-    if i == 2 * ((i // 10) * (i % 10)):
-        print(i, end=' ')
+# 05.08
+a = int(input())
+b = int(input())
+c = int(input())
+d = int(input())
+e = int(input())
+count = 0
+for i in range(1001):
+    if i - e != 0:
+        value = (a * i**3 + b * i**2 + c * i + d)
+        if value == 0:
+            count += 1
+print(count)
