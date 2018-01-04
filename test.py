da87bdaa41eeb16b6e813525d7932ae7dbf73037
zaichik666 <mary.a.sit@gmail.com>
2018-01-04 08:11:09 +0300

    05.09 сумма факториалов

--- test.py
+++ test.py
@@ -1,13 +1,11 @@
-# 05.08
-a = int(input())
-b = int(input())
-c = int(input())
-d = int(input())
-e = int(input())
-count = 0
-for i in range(1001):
-    if i - e != 0:
-        value = (a * i**3 + b * i**2 + c * i + d)
-        if value == 0:
-            count += 1
-print(count)
+# 05.09
+n = int(input())
+value = 0
+for i in range(1, n + 1):
+    if i == 1:
+        s = 1
+        value = 1
+    else:
+        s = s * i
+        value += s
+print(value)
