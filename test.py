4d5b8114971a554232fcc81e429684bdfbf17772
zaichik666 <mary.a.sit@gmail.com>
2018-01-10 21:51:30 +0300

    05.17 циклический сдвиг вправо

--- test.py
+++ test.py
@@ -1,13 +1,6 @@
-#05.16
+#05.17
 a = list(map(int, input().split()))
-b = []
-i = 0
-while i < len(a):
-    if i + 1 < len(a):
-        b.append(a[i+1])
-        b.append(a[i])
-
-    else:
-        b.append(a[i])
-    i += 2
+i = len(a) - 1
+b = [a[i]]
+b.extend(a[:i])
 print(*b)
