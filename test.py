3c7b135b4678a11827e6400208b5b54b07f6298e
zaichik666 <mary.a.sit@gmail.com>
2018-01-11 16:45:57 +0300

    05.19 найти максимальное произведение 3х элементов не прошло тест 5 =(

--- test.py
+++ test.py
@@ -1,18 +1,25 @@
-#05.18
+#05.19
 nList = list(map(int, input().split()))
-a = b = c = d = 0
+min1 = min2 = 0
+max0 = max1 = max2 = -30000
 for n in nList:
-    if n >= a:
-        b = a
-        a = n
-    if n < a and n > b:
-        b = n
-    if n <= c:
-        d = c
-        c = n
-    if n > c and n < d:
-        d = n
-if a * b > c * d:
-    print(b, a)
+    if n >= max0:
+        max2 = max1
+        max1 = max0
+        max0 = n
+    if n < max0 and n >= max1:
+        max2 = max1
+        max1 = n
+    if n < max1 and n > max2:
+        max2 = n
+    if n <= min1:
+        min2 = min1
+        min1 = n
+    if n > min1 and n < min2:
+        min2 = n
+if max0 == 0:
+    print(max1, min1, max0)
+elif max0 * max1 * max2 > max0 * min1 * min2:
+    print(max1, max2, max0)
 else:
-    print(c, d)
+    print(min1, min2, max0)
