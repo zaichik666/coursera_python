e4ef2883079b4a664933c0bb869535935d658ada
zaichik666 <mary.a.sit@gmail.com>
2018-01-12 22:26:20 +0300

    05.22 ферзи рабочая

--- test.py
+++ test.py
@@ -1,21 +1,23 @@
-#05.21
+#05.22
 i = 0
 ans = 'NO'
 tmpDown = []
 tmpUp = []
-Vertical = [1, 2, 3, 4, 5, 6, 7, 8]
-Horizontal = [1, 2, 3, 4, 5, 6, 7, 8]
+Vertical = []
+Horizontal = []
 while i < 8:
     x, y = tuple(map(int, input().split()))
+    if Vertical.count(x) == 0 and Horizontal.count(y) == 0:
+        Vertical.append(x)
+        Horizontal.append(y)
+    else:
+        ans = 'YES'
     y0_Down = y + x
     y0_Up = y - x
     if tmpDown.count(y0_Down) == 0 and tmpUp.count(y0_Up) == 0:
         tmpDown.append(y0_Down)
         tmpUp.append(y0_Up)
-    if Vertical.count(x) != 0 and Horizontal.count(y) != 0:
-        Vertical.remove(x)
-        Horizontal.remove(y)
     else:
-        ans == 'YES'
+        ans = 'YES'
     i += 1
 print(ans)
