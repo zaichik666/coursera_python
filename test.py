55e3ed7676fb698997e523afa73e6177a5fa13c9
zaichik666 <mary.a.sit@gmail.com>
2018-01-15 16:15:29 +0300

    06.03 обувной магазин

--- test.py
+++ test.py
@@ -1,20 +1,12 @@
-#06.02
-def Intersection(A, B):
-    i = j = 0
-    C = []
-    while i < len(A) and j < len(B):
-        if A[i] < B[j]:
-            i += 1
-        elif B[j] < A[i]:
-            j += 1
-        elif A[i] == B[j]:
-            C.append(A[i])
-            i += 1
-            j += 1
-    return C
-
-
-a = list(map(int, input().split()))
-b = list(map(int, input().split()))
-c = Intersection(a, b)
-print(*c)
+#06.03
+n = int(input())
+nList = list(map(int, input().split()))
+nList.sort()
+nCount = 0
+if nList.count(n) >= 1:
+    nCount = 1
+for item in nList:
+    if item > n and item >= n + 3:
+        nCount += 1
+        n = item
+print(nCount)
