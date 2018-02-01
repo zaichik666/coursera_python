b9759673dc31c2e7df8a0d8afa284d5fba64621b
zaichik666 <mary.a.sit@gmail.com>
2018-02-01 10:50:20 +0300

    06.07 сортировка подсчетом (клавиатура)

--- test.py
+++ test.py
@@ -1,6 +1,12 @@
-nums = list(map(int, input().split()))
-res = [0] * 101
-for num in nums:
-    res[num] += 1
-for nowRes in range(101):
-    print((str(nowRes) + ' ') * res[nowRes], end='')
+n = int(input())
+clicksMax = list(map(int, input().split()))
+k = int(input())
+clicks = list(map(int, input().split()))
+res = [0] * n
+for click in clicks:
+    res[click - 1] += 1
+for i in range(n):
+    if clicksMax[i] >= res[i]:
+        print('NO')
+    else:
+        print('YES')
